1119a15e903622d908979b8f4268a432ea896484
Dieg Luna <diegofcolunalopez@gmail.com>
2021-09-28 13:14:07 -0500

    Maquetación y estilos del flujo de pago

--- moonmakers-conf-merch/src/containers/Sucess.jsx
+++ moonmakers-conf-merch/src/containers/Sucess.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import '../styles/components/Success.css';
 
 function Sucess() {
   return (
-    <h1>
-      Sucess
-    </h1>
+    <div className="Success">
+      <div className="Success-content">
+        <h2>Diego, Gracias por tu compra</h2>
+        <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+        <div className="Success-map">
+          Google Maps
+        </div>
+      </div>
+    </div>
   )
 }
 
