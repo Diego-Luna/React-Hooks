0fba5b8db58f1f959dd03566dbea4ec4c55d3829
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-03 10:53:16 -0500

    React helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Helmet } from 'react-helmet';
 import AppContext from '../context/AppContex';
 
 import '../styles/components/Checkout.css';
@@ -22,34 +22,44 @@ function Checkout() {
   };
 
   return (
-    <div className="Checkout">
-      <div className="Checkout-content">
-        {cart.length > 0 ? <h3>Lusta de Pedidos</h3> : <h3>Sin pedidos...</h3>}
+    <>
+      <Helmet>
+        <title>Lista de pedidos - MoonMakers Conf Merch</title>
+      </Helmet>
 
-        {cart.map((item) => (
-          <div key={item.id} className="Checkout-item">
-            <div className="Checkout-element">
-              <h4>{item.title}</h4>
-              <span>${item.price}</span>
+      <div className="Checkout">
+        <div className="Checkout-content">
+          {cart.length > 0 ? (
+            <h3>Lusta de Pedidos</h3>
+          ) : (
+            <h3>Sin pedidos...</h3>
+          )}
+
+          {cart.map((item) => (
+            <div key={item.id} className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+
+              <button onClick={handleRemove(item)} type="button">
+                <i className="fas fa-trash-alt" title="Eliminar" />
+              </button>
             </div>
-
-            <button onClick={handleRemove(item)} type="button">
-              <i className="fas fa-trash-alt" title="Eliminar" />
-            </button>
-          </div>
-        ))}
-      </div>
-      {cart.length > 0 ? (
-        <div className="Checkout-sidebar">
-          <h3>Precio Total: ${handleSumTotal()} </h3>
-          <Link to="/checkout/informatio">
-            <button type="button">Continuar pedido</button>
-          </Link>
+          ))}
         </div>
-      ) : (
-        ''
-      )}
-    </div>
+        {cart.length > 0 ? (
+          <div className="Checkout-sidebar">
+            <h3>Precio Total: ${handleSumTotal()} </h3>
+            <Link to="/checkout/informatio">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
+        ) : (
+          ''
+        )}
+      </div>
+    </>
   );
 }
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+
 import Products from '../components/Products';
 import initialState from '../initialState';
 
 function Home() {
-  return <Products products={initialState.products} />;
+  return (
+    <>
+      <Helmet>
+        <title>MoonMakers Conf Merch - Productos</title>
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@MoonMakers" />
+        <meta name="twitter:creator" content="@MoonMakers" />
+        <meta name="twitter:title" content="MoonMakers Conf Merch" />
+        <meta name="twitter:description" content="MoonMakers Conf Merch" />
+        <meta
+          name="twitter:image"
+          content="https://i.pinimg.com/236x/e2/5e/4e/e25e4e4a848f569fa69554b896cc9543.jpg"
+        />
+        <meta property="og:title" content="MoonMakers Conf Merch" />
+        <meta property="og:description" content="MoonMakers Conf Merch" />
+        <meta
+          property="og:image"
+          content="https://i.pinimg.com/236x/e2/5e/4e/e25e4e4a848f569fa69554b896cc9543.jpg"
+        />
+        <meta property="og:url" content="moonmakers.org" />
+        <meta property="og:site_name" content="MoonMakers Conf Merch" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        {/* <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
+      </Helmet>
+      <Products products={initialState.products} />
+    </>
+  );
 }
 
 export default Home;
